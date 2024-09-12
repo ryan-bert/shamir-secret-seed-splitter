3644305bcff5be3be601463b5d18639195281a80
ryan-bert <ryanberti66@gmail.com>
2024-09-12 18:15:59 +0200

    Skeleton complete

--- src/main.py
+++ src/main.py
@@ -1,16 +1,35 @@
 
-# TODO: CLI for arguments
-mode = input("")
+def main():
 
-# TODO: If seed phrase, covert to master private key
-# - Validate seed phrase
-# - Convert to master private key using BIP39/BIP32
-# - Convert key to hex
+    # TODO: Mode selection (CLI)
+    mode = input("Select mode (split/reconstruct): ")
 
-# TODO: Validate private key 
+    if mode == "split":
+        split_mode()
+    elif mode == "reconstruct":
+        reconstruct_mode()
+    else:
+        raise ValueError("Invalid mode. Please select 'split' or 'reconstruct'.")
 
-# TODO: Split private key into shares using shamir.py
+def split_mode():
 
-# TODO: Reconstruct private key from shares using shamir.py to validate
 
-# TODO: Save shares to file(s) & print message to console
+    # TODO: If seed phrase, covert to master private key
+    # - Validate seed phrase
+    # - Convert to master private key using BIP39/BIP32
+    # - Convert key to hex
+
+    # TODO: Validate private key 
+
+    # TODO: Split private key into shares using shamir.py
+
+    # TODO: Reconstruct private key from shares using shamir.py to validate
+
+    # TODO: Save shares to file(s) & print message to console
+
+def reconstruct_mode():
+
+
+
+if __name__ == "__main__":
+    main()
