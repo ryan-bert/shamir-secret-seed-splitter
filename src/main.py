6d4634dcbf04d9174d2580c2448ccbf67c9d79bd
ryan-bert <ryanberti66@gmail.com>
2024-09-12 18:27:15 +0200

    skeleton for split_mode() done

--- src/main.py
+++ src/main.py
@@ -1,25 +1,29 @@
 
 def main():
 
-    # TODO: Mode selection (CLI)
+    # Mode selection (CLI)
     mode = input("Select mode (split/reconstruct): ")
-
+    # Mode validation
     if mode == "split":
-        split_mode()
+        split_mode()                # Split mode
     elif mode == "reconstruct":
-        reconstruct_mode()
+        reconstruct_mode()          # Reconstruct mode
     else:
         raise ValueError("Invalid mode. Please select 'split' or 'reconstruct'.")
 
 def split_mode():
 
+    # TODO: CLI for input type (private key/seed phrase) 
 
     # TODO: If seed phrase, covert to master private key
+    # - CLI for seed phrase input
     # - Validate seed phrase
     # - Convert to master private key using BIP39/BIP32
     # - Convert key to hex
 
     # TODO: Validate private key 
+    # - CLI for private key input 
+    # - Convert key to hex (ie validate hex characters)
 
     # TODO: Split private key into shares using shamir.py
 
