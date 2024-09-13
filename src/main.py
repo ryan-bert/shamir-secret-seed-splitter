22153d12a3c9eca6df31a91644a15b5c36e39f55
ryan-bert <ryanberti66@gmail.com>
2024-09-13 15:02:58 +0200

    mode & type selection implemented

--- src/main.py
+++ src/main.py
@@ -2,18 +2,27 @@
 def main():
 
     # Mode selection (CLI)
-    mode = input("Select mode (split/reconstruct): ")
+    mode = input("Select mode - split (s)/reconstruct (r): ")
     # Mode validation
-    if mode == "split":
-        split_mode()                # Split mode
-    elif mode == "reconstruct":
-        reconstruct_mode()          # Reconstruct mode
+    if mode == "split" or mode == "s":
+        split_mode()                                # Split mode
+    elif mode == "reconstruct" or mode == "r":
+        reconstruct_mode()                          # Reconstruct mode
     else:
         raise ValueError("Invalid mode. Please select 'split' or 'reconstruct'.")
 
 def split_mode():
 
-    # TODO: CLI for input type (private key/seed phrase) 
+
+    # Input type selection (CLI)
+    input_type = input("Select input type - private key (p)/seed phrase (s): ")
+    # Input type validation
+    if input_type == "private key" or input_type == "p":
+        input_type = "private key"
+    elif input_type == "seed phrase" or input_type == "s":
+        input_type = "seed phrase"
+    else:
+        raise ValueError("Invalid input type. Please select 'private key' or 'seed phrase'.")
 
     # TODO: If seed phrase, covert to master private key
     # - CLI for seed phrase input
